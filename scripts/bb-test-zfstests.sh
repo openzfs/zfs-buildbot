ffebc3536494fac038c912cd2b085dbf3a59bc9f
Brian Behlendorf <behlendorf1@llnl.gov>
2016-02-26 16:55:29 -0800

    Add bb-test-zfstests.sh
    A wrapper script for buildbot to launch the ZFS Test Suite.  For
    the moment this test suite is disabled by default.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

---
+++ scripts/bb-test-zfstests.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Check for a local cached configuration.
+if test -f /etc/buildslave; then
+    . /etc/buildslave
+fi
+
+# Custom test options will be saved in the tests directory.
+if test -f "../TEST"; then
+    . ../TEST
+fi
+
+TEST_ZFSTESTS_SKIP=${TEST_ZFSTESTS_SKIP:-"Yes"}
+if echo "$TEST_ZFSTESTS_SKIP" | grep -Eiq "^yes$|^on$|^true$|^1$"; then
+    echo "Skipping disabled test"
+    exit 3
+fi
+
+ZFS_SH=${ZFS_SH:-"zfs.sh"}
+ZFSTESTS=${ZFSTESTS:-"zfs-tests.sh"}
+CONSOLE_LOG="$PWD/console.log"
+
+# Cleanup the pool and restore any modified system state.  The console log
+# is dumped twice to maximize the odds of preserving debug information.
+cleanup()
+{
+    dmesg >$CONSOLE_LOG
+    sudo -E $ZFS_SH -u
+    dmesg >$CONSOLE_LOG
+}
+trap cleanup EXIT SIGTERM
+
+set -x
+
+TEST_ZFSTESTS_DISKS=${TEST_ZFSTESTS_DISKS:-""}
+TEST_ZFSTESTS_DISKSIZE=${TEST_ZFSTESTS_SIZE:-"2G"}
+TEST_ZFSTESTS_OPTIONS=${TEST_ZFSTESTS_OPTIONS:-"-v"}
+TEST_ZFSTESTS_RUNFILE=${TEST_ZFSTESTS_RUNFILE:-"linux.run"}
+
+if [ -n "$TEST_ZFSTESTS_DISKS" ]; then
+	DISKS=${TEST_ZFSTESTS_DISKS}
+	export DISKS
+fi
+
+set +x
+
+sudo -E dmesg -c >/dev/null
+sudo -E $ZFS_SH || exit 1
+$ZFSTESTS $TEST_ZFSTESTS_OPTIONS -s $TEST_ZFSTESTS_DISKSIZE \
+    -r $TEST_ZFSTESTS_RUNFILE &
+CHILD=$!
+
+sleep 1
+TEST_LOG=$(ls -t /var/tmp/test_results/*/log | head -1)
+rm -f log
+ln -s $TEST_LOG/log log
+
+wait $CHILD
+RESULT=$?
+
+exit $RESULT
