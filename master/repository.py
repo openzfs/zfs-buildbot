1e3e4dd11584e9ba2383148cad0f1743bb8bff7b
Neal Gompa (ニール・ゴンパ) <ngompa13@gmail.com>
2018-11-12 13:42:28 -0500

    Move repository definitions to a separate file (#168)
    This centralizes the configuration of the repository sources in
    a single file, similar to the passwords. In cases where other
    Buildbot configuration modules need access to these settings, it
    ensures we can avoid issues with circular imports.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: George Melikov <mail@gmelikov.ru>
    Signed-off-by: Neal Gompa <ngompa@datto.com>

---
+++ master/repository.py
@@ -0,0 +1,18 @@
+# -*- python -*-
+# ex: set syntax=python:
+
+spl_repo = "https://github.com/zfsonlinux/spl.git"
+zfs_repo = "https://github.com/zfsonlinux/zfs.git"
+linux_repo = "https://github.com/torvalds/linux.git"
+lustre_repo = "git://git.hpdd.intel.com/fs/lustre-release.git"
+
+all_repositories = {
+    "https://github.com/torvalds/linux" : 'linux',
+    "git://git.hpdd.intel.com/fs/lustre-release" : 'lustre',
+    "https://github.com/zfsonlinux/spl" : 'spl',
+    "https://github.com/zfsonlinux/zfs" : 'zfs',
+    "https://github.com/torvalds/linux.git" : 'linux',
+    "git://git.hpdd.intel.com/fs/lustre-release.git" : 'lustre',
+    "https://github.com/zfsonlinux/spl.git" : 'spl',
+    "https://github.com/zfsonlinux/zfs.git" : 'zfs',
+}
