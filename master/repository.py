aa344b1c97146b106fa3285c16e4ac4acd8f3899
Tony Hutter <hutter2@llnl.gov>
2020-06-16 20:53:59 +0000

    Update buildbot to 3.2.0
    This patch does a number of things to update zfs-buildbot from
    buildbot 0.8.x to 3.2.0.
    
    - Remove old patchsets
    - Use "worker" terminology to match new buildbot APIs.
    - Make a bunch of previously hard-coded global variables optionally
      set-able in password.py (for testing only).
    - Correctly name the coverage tester Ubuntu 17 instead of 18
    - Add nginx config to README.md and setup instructions for buildbot.
    - Fix get_freebsd_image(): 'NoneType' object has no attribute 'get_image'
    - Base64 encode user_data to get around error
    - Update block_device_map
    - Integrate isIdle() patch into zfs-buildbot.  We no longer have an
      idle worker state, so just check to see if a builder is available.
    - Get rid of: "logHorizon/buildHorizon is deprecated" warnings
    - Get rid of special chars in worker names (like '.').  This gets
      around an error:
           "worker name '<worker name>' is not an identifier
    - Fix auth/authz/www stuff
    - Fix 'nextWorker now takes a 3rd argument'
    - Fix error while parsing config file: cannot import name 'html'
    - mergeRequests -> collapseRequests
    - Make boot_device_map a list to match new API
    - Fix "builtins.TypeError: __init__() takes at least 3 arguments"
    - buildbot.status.results -> buildbot.process.results
    - Misc other updates
    - Remove 'lazylogfiles' due to buildbot bug:
      https://github.com/buildbot/buildbot/issues/5957
    - Remove all patches except "Enable-run-time-AMI-determination.patch"
      since most of them have already been baked into upstream buildbot.
    
    Signed-off-by: Tony Hutter <hutter2@llnl.gov>

--- master/repository.py
+++ master/repository.py
@@ -1,7 +1,16 @@
 # -*- python -*-
 # ex: set syntax=python:
 
-zfs_repo = "https://github.com/openzfs/zfs.git"
+from password import *
+
+# During testing we may want to test against our own repo (like "tonyhutter")
+# rather than "openzfs".  If the user has dev_repo_owner set in passwd.py, then
+# use it here
+try: dev_repo_owner
+except NameError: repo_owner = "openzfs"
+else: repo_owner = dev_repo_owner
+
+zfs_repo = "https://github.com/" + repo_owner + "/zfs.git"
 linux_repo = "https://github.com/torvalds/linux.git"
 
 all_repositories = {
@@ -9,4 +18,8 @@ all_repositories = {
     "https://github.com/openzfs/zfs" : 'zfs',
     "https://github.com/torvalds/linux.git" : 'linux',
     "https://github.com/openzfs/zfs.git" : 'zfs',
+    "git://git.hpdd.intel.com/fs/lustre-release" : 'lustre',
+    "https://github.com/" + repo_owner + "/zfs" : 'zfs',
+    "git://git.hpdd.intel.com/fs/lustre-release.git" : 'lustre',
+    "https://github.com/" + repo_owner + "/zfs.git" : 'zfs',
 }
