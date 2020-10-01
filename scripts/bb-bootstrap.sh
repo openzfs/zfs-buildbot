28fcb4b32d8405b26965be15cb0d87ec32b7b7be
Jonas Bergler <jbergler@meraki.com>
2020-10-02 07:58:31 +1300

    bb-bootstrap, create $BB_DIR/info
    I'm not sure if this is an artifact of how I was testing or if there is
    actually something else that was missing, either way this is a safe way
    to ensure that info directory exists for the following code that fetches
    instance metadata.

--- scripts/bb-bootstrap.sh
+++ scripts/bb-bootstrap.sh
@@ -469,7 +469,7 @@ fi
 
 # Generic buildslave configuration
 if test ! -d $BB_DIR; then
-    mkdir -p $BB_DIR
+    mkdir -p $BB_DIR/info
     chown buildbot:buildbot $BB_DIR
     sudo -E -u buildbot $BUILDSLAVE create-slave --umask=022 --usepty=0 $BB_PARAMS
 fi
