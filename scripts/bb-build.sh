97a4027354d27be354dbc0a5a037be36bdf97763
Ryan Moeller <ryan@iXsystems.com>
2021-02-04 11:55:36 -0500

    Build with INVARIANTS on FreeBSD 14 only

--- scripts/bb-build.sh
+++ scripts/bb-build.sh
@@ -7,7 +7,7 @@ fi
 case "$BB_NAME" in
 FreeBSD*)
 	MAKE="gmake WITH_DEBUG=true"
-	if expr $(freebsd-version -k) : "13.0-" >/dev/null; then
+	if expr $(freebsd-version -k) : "14.0-" >/dev/null; then
 		MAKE="$MAKE WITH_INVARIANTS=true"
 	fi
 	NCPU=$(sysctl -n hw.ncpu)
