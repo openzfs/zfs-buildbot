7d901225ab5d71a359c192bce3ec314a6e2f3e70
Brian Behlendorf <behlendorf1@llnl.gov>
2021-02-18 17:37:38 -0800

    Retire Ubuntu builders
    The buildbot Ubuntu builders have been migrated to Github Actions.
    For the moment we're leaving support for them in place if we decide
    to reenable them because there's an issue with Actions.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- master/github.py
+++ master/github.py
@@ -13,7 +13,7 @@ from dateutil.parser import parse as dateparse
 from twisted.python import log
 
 builders_common="arch,"
-builders_linux="centos7,centos8,centosstream8,debian10,fedora33,ubuntu18,ubuntu20,builtin,"
+builders_linux="centos7,centos8,centosstream8,debian10,fedora33,builtin,"
 builders_freebsd="freebsd12,freebsd13,"
 
 builders_push_master=builders_common+builders_linux+builders_freebsd+"coverage"
