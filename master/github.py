fb6e243c63493ab8112ccd16b048b5ff14f9ca9e
Brian Behlendorf <behlendorf1@llnl.gov>
2021-04-11 14:36:24 -0700

    Enable FreeBSD 14 builder
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- master/github.py
+++ master/github.py
@@ -14,7 +14,7 @@ from twisted.python import log
 
 builders_common="arch,"
 builders_linux="centos7,centos8,centosstream8,debian10,fedora33,builtin,"
-builders_freebsd="freebsd12,freebsd13,"
+builders_freebsd="freebsd12,freebsd13,freebsd14"
 
 builders_push_master=builders_common+builders_linux+builders_freebsd+"coverage"
 builders_push_release=builders_common+builders_linux+builders_freebsd
